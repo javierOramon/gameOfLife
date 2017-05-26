8b0b5281bf729c04db8c843f2e3ea66371255c88
Javier Garcia <javier.garcia@oramon.nl>
2017-05-26 17:01:37 +0200

    Check the width of the board is correct

--- src/test/java/javi/katas/gameoflife/Board.java
+++ src/test/java/javi/katas/gameoflife/Board.java
@@ -3,13 +3,19 @@ package javi.katas.gameoflife;
 public class Board {
 
     private final Integer rows;
+    private final Integer columns;
 
-    public Board(Integer rows) {
+    public Board(Integer rows, Integer columns) {
 
         this.rows = rows;
+        this.columns = columns;
     }
 
     public Integer rows() {
         return rows;
+    }
+
+    public Integer columns() {
+        return columns;
     }
 }
--- src/test/java/javi/katas/gameoflife/BoardTest.java
+++ src/test/java/javi/katas/gameoflife/BoardTest.java
@@ -16,10 +16,21 @@ public class BoardTest {
     public void checkHeighOfBoard(){
         //Arrange
         final Integer rows = 5;
-        Board board = new Board(rows);
+        Board board = new Board(rows, 0);
         //Act
         Integer numberOfRows = board.rows();
         //Assert
         assertEquals("The number of rows is not the same which was injected", rows, numberOfRows);
+    }
+
+    @Test
+    public void checkWidthOfBoard(){
+        //Arrange
+        final Integer columns = 5;
+        Board board = new Board(0, columns);
+        //Act
+        Integer numberOfRows = board.columns();
+        //Assert
+        assertEquals("The number of columns is not the same which was injected", columns, numberOfRows);
     }
 }

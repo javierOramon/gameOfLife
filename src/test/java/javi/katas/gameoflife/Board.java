bb4238ab7ecc80bc4366026ea874e502135cb884
Javier Garcia <javier.garcia@oramon.nl>
2017-05-26 16:58:49 +0200

    Resolved test to get the rows properly

--- src/test/java/javi/katas/gameoflife/Board.java
+++ src/test/java/javi/katas/gameoflife/Board.java
@@ -2,11 +2,14 @@ package javi.katas.gameoflife;
 
 public class Board {
 
+    private final Integer rows;
+
     public Board(Integer rows) {
 
+        this.rows = rows;
     }
 
     public Integer rows() {
-        return null;
+        return rows;
     }
 }

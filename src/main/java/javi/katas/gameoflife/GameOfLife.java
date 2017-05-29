86511939f0d58552a4bb523b18c4d97c3217f1b4
Javier Garcia <javier.garcia@oramon.nl>
2017-05-29 13:32:22 +0200

    Added test to recover the current board

---
+++ src/main/java/javi/katas/gameoflife/GameOfLife.java
@@ -0,0 +1,14 @@
+package javi.katas.gameoflife;
+
+public class GameOfLife {
+    public GameOfLife(Board board) {
+    }
+
+    public void execute() {
+
+    }
+
+    public Board recoverBoard() {
+        return null;
+    }
+}
---
+++ src/test/java/javi/katas/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,39 @@
+package javi.katas.gameoflife;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameOfLifeTest {
+
+    private Board board;
+    private GameOfLife gameOfLife;
+
+    @Before
+    public void setUp(){
+        board = new Board(5,5);
+        gameOfLife = new GameOfLife(board);
+    }
+
+    @Test
+    public void recoverBoardReturnsTheCurrentBoard(){
+        //Arrange
+        //Act
+        Board boardResult = gameOfLife.recoverBoard();
+        //Assert
+        assertEquals("Is not the same board", board, boardResult);
+    }
+
+    @Test
+    public void cellWithoutNeighboursDie(){
+        //Arrange
+        Board board = new Board(5,5);
+        final Coordinate any = new Coordinate(0, 0);
+        board.changeCell(any, Cell.ALIVE);
+        //Act
+        gameOfLife.execute();
+        //Assert
+        Board boardResult = gameOfLife.recoverBoard();
+    }
+}

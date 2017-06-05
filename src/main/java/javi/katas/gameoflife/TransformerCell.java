dc2b84facdf16a8e4c38ed7171db0bc2dce236b8
Javier Garcia <javier.garcia@oramon.nl>
2017-06-05 10:30:07 +0200

    Passes test of revive cells

--- src/main/java/javi/katas/gameoflife/TransformerCell.java
+++ src/main/java/javi/katas/gameoflife/TransformerCell.java
@@ -7,19 +7,8 @@ public class TransformerCell {
         long amountLives = aroundCells.neightbours().stream().filter(cell -> Cell.ALIVE.equals(cell)).count();
         if(amountLives < 2 || amountLives == 4){
             board.changeCell(center, Cell.DIE);
+        }else if(amountLives == 3){
+            board.changeCell(center, Cell.ALIVE);
         }
     }
-
-//    public void execute(int range) {
-//        for (int row = 0; row < board.rows(); row++){
-//            for(int column = 0; column < board.columns(); column++){
-//                final Coordinate centerCell = new Coordinate(row, column);
-//                AroundCells aroundCells = board.neightbours(centerCell, range);
-//                long amountLives = aroundCells.neightbours().stream().filter(cell -> Cell.ALIVE.equals(cell)).count();
-//                if(amountLives < 2 || amountLives == 4){
-//                    board.changeCell(centerCell, Cell.DIE);
-//                }
-//            }
-//        }
-//    }
 }

9efec6674f953987fe4530d79a51bc7e10fc206d
Javier Garcia <javier.garcia@oramon.nl>
2017-06-05 10:29:20 +0200

    Added test to revive with three neightbours alive

--- src/test/java/javi/katas/gameoflife/TransformerCellTest.java
+++ src/test/java/javi/katas/gameoflife/TransformerCellTest.java
@@ -95,4 +95,18 @@ public class TransformerCellTest {
         Cell cellResult = board.recoverCell(center);
         assertEquals("The cell is death", Cell.ALIVE, cellResult);
     }
+
+    @Test
+    public void cellDeathWithThreeNeightboursRevive(){
+        //Arrange
+        final Coordinate center = new Coordinate(2, 2);
+        board.changeCell(new Coordinate(1,1), Cell.ALIVE);
+        board.changeCell(new Coordinate(1,2), Cell.ALIVE);
+        board.changeCell(new Coordinate(1,3), Cell.ALIVE);
+        //Act
+        transformerCell.transform(center, board, 1);
+        //Assert
+        Cell cellResult = board.recoverCell(center);
+        assertEquals("The cell is death", Cell.ALIVE, cellResult);
+    }
 }

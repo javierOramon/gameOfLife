1cfef6021a5988f77d5c6a06d1ad2c3e19f34c79
Javier Garcia <javier.garcia@oramon.nl>
2017-06-06 09:34:42 +0200

    Refactor in transformCell

--- src/main/java/javi/katas/gameoflife/TransformerCell.java
+++ src/main/java/javi/katas/gameoflife/TransformerCell.java
@@ -4,11 +4,20 @@ public class TransformerCell {
 
     public void transform(Coordinate center, Board board, Integer range){
         AroundCells aroundCells = board.neightbours(center, range);
-        long amountLives = aroundCells.neightbours().stream().filter(cell -> Cell.ALIVE.equals(cell)).count();
-        if(amountLives < 2 || amountLives == 4){
-            board.changeCell(center, Cell.DIE);
-        }else if(amountLives == 3){
-            board.changeCell(center, Cell.ALIVE);
-        }
+        Integer amountLives = Math.toIntExact(aroundCells.neightbours().stream().filter(cell -> Cell.ALIVE.equals(cell)).count());
+        killCell(amountLives, center, board);
+        reviveCell(amountLives, center, board);
+    }
+
+    private void killCell(Integer amountLives, Coordinate center, Board board){
+        if(amountLives >= 2 && amountLives != 4) return;
+
+        board.changeCell(center, Cell.DIE);
+    }
+
+    private void reviveCell(Integer amountLives, Coordinate center, Board board){
+        if(amountLives != 3) return;
+
+        board.changeCell(center, Cell.ALIVE);
     }
 }

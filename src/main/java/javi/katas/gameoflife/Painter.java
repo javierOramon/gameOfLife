a24173220b523937889f181cbab3e04516be7ffe
Javier Garcia <javier.garcia@oramon.nl>
2017-06-13 10:09:46 +0200

    Added test to paint a board

---
+++ src/main/java/javi/katas/gameoflife/Painter.java
@@ -0,0 +1,11 @@
+package javi.katas.gameoflife;
+
+public class Painter {
+    public Painter(PainterCell painterCell) {
+
+    }
+
+    public String paint(Board board) {
+        return null;
+    }
+}
---
+++ src/test/java/javi/katas/gameoflife/PainterTest.java
@@ -0,0 +1,27 @@
+package javi.katas.gameoflife;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PainterTest {
+
+    private Painter painter;
+
+    @Before
+    public void setUp(){
+        painter = new Painter(new PainterCell());
+    }
+
+    @Test
+    public void printEmptyBoard(){
+        //Arrange
+        final String expected = "**\n**";
+        Board board = new Board(2,2);
+        //Act
+        String result = painter.paint(board);
+        //Assert
+        assertEquals("The result is not correct", expected, result);
+    }
+}
